38ac614333c5976ce26323db75c8468350b63573
zuspect <artemmishenko@mail.ru>
2024-05-24 00:06:21 +0400

    fix fonts again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {
-        enabled: true
+        enabled: true,
     },
-    modules: [
-        '@nuxtjs/tailwindcss'
-    ],
+    modules: ['@nuxtjs/tailwindcss', ['@nuxtjs/google-fonts', {
+        families: {
+            Unbounded: '200..800',
+            'Inter Tight': '200..800',
+        },
+    }]],
     css: ['~/assets/scss/main.scss'],
     postcss: {
         plugins: {

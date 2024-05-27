fbb26ae0e8fb83b93b8fcddb73f1bc74b0090bfb
zuspect <artemmishenko@mail.ru>
2024-05-27 23:54:44 +0400

    team section

--- app.vue
+++ app.vue
@@ -3,7 +3,8 @@
     <Stats class='pb-container' />
     <Steps class='py-container' />
     <Services class='py-container' />
-    <Clients class='py-container' />
+    <Clients class='pt-container' />
+    <Team />
 </template>
 <script setup lang='ts'>
 import ForWho from '~/components/sections/ForWho.vue'
@@ -11,4 +12,5 @@ import Stats from '~/components/sections/Stats.vue'
 import Steps from '~/components/sections/Steps.vue'
 import Services from '~/components/sections/Services.vue'
 import Clients from '~/components/sections/Clients.vue'
+import Team from '~/components/sections/Team.vue'
 </script>
---
+++ components/sections/Team.vue
@@ -0,0 +1,62 @@
+<template>
+    <Section class='bg-light'>
+        <Container class='grid grid-cols-1 lg:grid-cols-2 gap-container'>
+            <div v-for='(card,key) in cards' :key='key' class='flex flex-col'>
+                <div class='flex'>
+                    <h2 class='unbounded text-5xl mb-default font-bold' :class='`text-${card.color}`'>
+                        {{ card.title }}
+                    </h2>
+                    <div class='border-b flex-grow mb-[1.6rem] ml-[-0.5rem]' :class='`border-${card.color}`' />
+                </div>
+                <div
+                    class='flex-grow bg-white rounded-default shadow-default grid grid-cols-3 lg:grid-cols-2 gap-container overflow-hidden'>
+                    <img alt='' :src='card.src' class='object-cover w-full h-full' />
+                    <!--                    <div class='flex flex-grow' :style='`background: url(${card.src})`'></div>-->
+                    <div class='flex flex-col gap-default py-default col-span-2 lg:col-span-1'>
+                        <h3 class='text-2xl font-bold'>{{ card.fullname }}</h3>
+                        <span class='text-secondary text-xl font-light border-b border-grey pb-small'>
+                            {{ card.position }}
+                        </span>
+                        <p v-for='(text,key) in card.exp' :key='key' class='pr-small'>{{ text }}</p>
+                    </div>
+                </div>
+            </div>
+        </Container>
+    </Section>
+</template>
+
+<script setup lang='ts'>
+import Container from '~/components/Container.vue'
+import Section from '~/components/Section.vue'
+
+const cards = [
+    {
+        title: 'наша',
+        color: 'dark',
+        fullname: 'Михаил Масляков',
+        position: 'Бизнес-тренер',
+        src: '/images/team_1.png',
+        exp: [
+            '5 лета опыта в тренерской деятельности',
+            'Работал с HeadHunter, Актион-МЦФР',
+            'Обучался у Сергея Филиппова, Игоря Рызова, Михаила Гребенюка',
+            'Разработал метод найма, который повысил «оставаемость» стажеров в 5 раз',
+        ],
+    },
+    {
+        title: 'команда',
+        color: 'primary',
+        fullname: 'Артём Мищенко',
+        position: 'IT-специалист',
+        src: '/images/team_2.png',
+        exp: [
+            '6 лет опыта в разработке веб-сервисов и приложений',
+            'Работал в дочерней компании корпорации “Росатом”',
+            'Разработал телеграм-бота для образовательного форума “Территория смыслов” 2023',
+        ],
+    },
+]
+</script>
+
+<style lang='scss' scoped>
+</style>

b5141b2fb93f8cff41d287772ea573ab6c2d10da
zuspect <artemmishenko@mail.ru>
2024-05-28 17:33:25 +0400

    mask

--- components/sections/Action.vue
+++ components/sections/Action.vue
@@ -1,15 +1,20 @@
 <template>
     <Section class='bg-light'>
         <Container>
-            <div class='p-default rounded-default bg-dark shadow-default grid grid-cols-1 lg:grid-cols-2 items-center'>
-                <h2 class='unbounded text-5xl mb-default font-bold p-container text-white'>
+            <div
+                class='p-default rounded-default bg-dark shadow-default grid grid-cols-1 lg:grid-cols-2 items-center gap-container'>
+                <h2 class='unbounded text-5xl mb-default font-bold lg:p-container text-white text-center ml-[-4rem]'>
                     остались<br><span class='text-white pl-[5rem]'>вопросы?</span>
                 </h2>
-                <div class='flex flex-col'>
+                <div class='flex flex-col lg:px-container'>
                     <span class='text-light'>Оставьте заявку на бесплатную презентацию, на которой вы узнаете: как проходит обучение и каких финансовых показателей вы достигнете.</span>
-                    <div class='flex py-default gap-container'>
-                        <input class='input' placeholder='+7 (987) 654-32-10' />
-                        <input class='input' placeholder='Ваше Имя' />
+                    <div class='pt-default gap-default lg:gap-container grid grid-cols-2'>
+                        <MaskInput
+                            v-model='phone'
+                            class='input' mask='+7 (###) ###-##-##'
+                            placeholder='+7 (987) 654-32-10'
+                        />
+                        <!--                        <input v-model='name' class='input' placeholder='Ваше Имя' />-->
                         <Button label='Оставить заявку' primary />
                     </div>
                 </div>
@@ -22,6 +27,10 @@
 import Container from '~/components/Container.vue'
 import Section from '~/components/Section.vue'
 import Button from '~/components/buttons/Button.vue'
+import { MaskInput } from 'vue-3-mask'
+
+const phone = ref()
+const name = ref()
 </script>
 
 <style lang='scss' scoped>

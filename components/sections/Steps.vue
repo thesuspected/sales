54d82379e6ff9b938a1e72a3fe3c403f17650a76
zuspect <artemmishenko@mail.ru>
2024-06-05 19:06:46 +0400

    fix ? sign in steps

--- components/sections/Steps.vue
+++ components/sections/Steps.vue
@@ -1,33 +1,34 @@
 <template>
-    <Section class='bg-light'>
+    <Section class="bg-light">
         <Container>
-            <div class='flex flex-col'>
-                <h1 class='title unbounded font-bold text-4xl md:text-5xl text-center mx-auto'>
-                    Как мы увеличиваем<br> <span class='text-primary'>показатели продаж</span>
-                    <img src='/svg/picture_question.svg' alt='' class='title-picture hide md:block' />
+            <div class="flex flex-col">
+                <h1 class="title unbounded font-bold text-4xl md:text-5xl text-center mx-auto">
+                    Как мы увеличиваем<br />
+                    <span class="text-primary">показатели продаж<span v-if="quasar.screen.lt.md">?</span></span>
+                    <img src="/svg/picture_question.svg" alt="" class="title-picture hide md:block" />
                 </h1>
-                <span class='uppercase text-pre-dark font-light text-center mx-auto pt-default'>
-                    Для этого существует
-                    понятный план
-                    действий
+                <span class="uppercase text-pre-dark font-light text-center mx-auto pt-default">
+                    Для этого существует понятный план действий
                 </span>
             </div>
             <Step1 />
-            <Step2 class='mt-section' />
+            <Step2 class="mt-section" />
             <Step3 />
         </Container>
     </Section>
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
 import Section from '~/components/Section.vue'
 import Container from '~/components/Container.vue'
 import Step1 from '~/components/sections/steps/Step1.vue'
 import Step2 from '~/components/sections/steps/Step2.vue'
 import Step3 from '~/components/sections/steps/Step3.vue'
+
+const quasar = useQuasar()
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .title {
     position: relative;
 

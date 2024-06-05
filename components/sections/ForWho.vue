f8183a73aa448ab09cbfdf6c3365d81b475e00ad
zuspect <artemmishenko@mail.ru>
2024-06-05 17:52:38 +0400

    add swiper

--- components/sections/ForWho.vue
+++ components/sections/ForWho.vue
@@ -3,28 +3,41 @@
         <Container>
             <Headline>
                 <template #title>
-                    Кому необходимы<br> наши услуги?
+                    Кому необходимы<br />
+                    наши услуги?
                 </template>
                 <template #subtitle>
-                    Наша миссия — увеличение доходов компаний<br> за счёт повышения профессионализма,<br> вовлеченности
-                    и счастья сотрудников.
+                    Наша миссия — увеличение доходов компаний<br />
+                    за счёт повышения профессионализма,<br />
+                    вовлеченности и счастья сотрудников.
                 </template>
             </Headline>
-            <div class='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-container'>
-                <div class='card' v-for='card in cards'>
-                    <img :src='card.src' alt='' />
-                    <span v-html='card.text' />
-                </div>
+            <!--            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-container">-->
+            <!--                <div class="card" v-for="card in cards">-->
+            <!--                    <img :src="card.src" alt="" />-->
+            <!--                    <span v-html="card.text" />-->
+            <!--                </div>-->
+            <!--            </div>-->
+            <div class="swiper-container mt-container">
+                <swiper :slides-per-view="quasar.screen.lt.md ? 1.3 : 4" :space-between="16" class="base-swiper">
+                    <swiper-slide class="card" v-for="card in cards">
+                        <img :src="card.src" alt="" />
+                        <span v-html="card.text" />
+                    </swiper-slide>
+                </swiper>
             </div>
         </Container>
     </Section>
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
 import Container from '~/components/Container.vue'
 import Section from '~/components/Section.vue'
 import Headline from '~/components/Headline.vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
 
+const quasar = useQuasar()
 const cards = [
     {
         src: '/svg/picture_1.svg',
@@ -45,11 +58,22 @@ const cards = [
 ]
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .card {
-    @apply bg-light text-dark p-5 rounded-default gap-default;
+    @apply bg-light text-dark p-5 rounded-default gap-default h-full;
     display: flex;
     flex-direction: column;
     align-items: center;
+    min-height: 370px;
+}
+
+.swiper-container {
+    margin-left: -1.25rem;
+    margin-right: -1.25rem;
+
+    .base-swiper {
+        padding-left: 1.25rem;
+        padding-right: 1.25rem;
+    }
 }
 </style>

8990ef7d4834194d599d4c56a242ea3a57335f5f
zuspect <artemmishenko@mail.ru>
2024-06-05 18:01:33 +0400

    generate month names

--- components/sections/steps/Step2.vue
+++ components/sections/steps/Step2.vue
@@ -1,48 +1,55 @@
 <template>
-    <div class='grid grid-cols-1 lg:grid-cols-3 gap-container'>
-        <div class='col-span-2 flex flex-col'>
-            <h2 class='unbounded text-5xl mb-default font-bold'>шаг 2</h2>
-            <div class='grid grid-cols-1 md:grid-cols-3 gap-container flex-grow'>
-                <div class='education-card card bg-white col-span-3 md:col-span-2 shadow-default'>
-                    <div class='flex flex-col gap-default education-card-inner flex-grow'>
-                        <div class='flex items-center gap-default text-primary size-30px'>
-                            <img src='~/assets/icons/step_edu.svg' alt='' />
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-container">
+        <div class="col-span-2 flex flex-col">
+            <h2 class="unbounded text-5xl mb-default font-bold">шаг 2</h2>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-container flex-grow">
+                <div class="education-card card bg-white col-span-3 md:col-span-2 shadow-default">
+                    <div class="flex flex-col gap-default education-card-inner flex-grow">
+                        <div class="flex items-center gap-default text-primary size-30px">
+                            <img src="~/assets/icons/step_edu.svg" alt="" />
                             Обучающая платформа
                         </div>
-                        <span class='md:w-10/12 pb-default'>
-                        По окончанию тренинга создаем <b class='text-primary'>командный чат</b> и добавляем участников на обучающую платформу. Мы будем отслеживать выполнение домашних заданий, давать обратную связь, обмениваться опытом и достижениями на протяжении трёх месяцев.
-                        <br><br>
-                        Настрой и показатели в команде будут постепенно улучшаться. В любой момент вы сможете посмотреть отчеты по сотрудникам и вносить коррекции в их работу. 
-                    </span>
+                        <span class="md:w-10/12 pb-default">
+                            По окончанию тренинга создаем <b class="text-primary">командный чат</b> и добавляем
+                            участников на обучающую платформу. Мы будем отслеживать выполнение домашних заданий, давать
+                            обратную связь, обмениваться опытом и достижениями на протяжении трёх месяцев. <br /><br />
+                            Настрой и показатели в команде будут постепенно улучшаться. В любой момент вы сможете
+                            посмотреть отчеты по сотрудникам и вносить коррекции в их работу. 
+                        </span>
                     </div>
                 </div>
-                <div class='hide md:block'>
-                    <div class='card bg-white month-card h-full shadow-default'>
-                        <div class='flex items-center justify-center gap-default size-30px mb-small'>
-                            3 месяца
-                        </div>
-                        <span v-for='advantage in educationAdvantages'
-                              class='border-t last:border-b border-grey py-small'>{{ advantage
-                            }}</span>
+                <div class="hide md:block">
+                    <div class="card bg-white month-card h-full shadow-default">
+                        <div class="flex items-center justify-center gap-default size-30px mb-small">3 месяца</div>
+                        <span
+                            v-for="advantage in educationAdvantages"
+                            class="border-t last:border-b border-grey py-small"
+                            >{{ advantage }}</span
+                        >
                     </div>
                 </div>
             </div>
-            <div class='timeline-card shadow-default'>
-                <div class='grid grid-cols-3 flex-grow'>
-                    <div v-for='month in months' class='timeline-card-month'>{{ month }}</div>
+            <div class="timeline-card shadow-default">
+                <div class="grid grid-cols-3 flex-grow">
+                    <div v-for="month in currentMonths" class="timeline-card-month">{{ month }}</div>
                 </div>
-                <div class='timeline-card-hr'></div>
+                <div class="timeline-card-hr"></div>
             </div>
         </div>
-        <div class='mockup'>
-            <img class='image' src='~/assets/images/mockup.png' alt='' />
-            <m-btn label='Оставить заявку на тренинг' class='shadow-default' icon-right='arrow_forward' full-width
-                   large />
+        <div class="mockup">
+            <img class="image" src="~/assets/images/mockup.png" alt="" />
+            <m-btn
+                label="Оставить заявку на тренинг"
+                class="shadow-default"
+                icon-right="arrow_forward"
+                full-width
+                large
+            />
         </div>
     </div>
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
 import MBtn from '~/components/buttons/MBtn.vue'
 
 const educationAdvantages = [
@@ -53,13 +60,25 @@ const educationAdvantages = [
     'Обмен опытом',
 ]
 const months = [
+    'январь',
+    'февраль',
     'март',
     'апрель',
     'май',
+    'июнь',
+    'июль',
+    'август',
+    'сентябрь',
+    'октябрь',
+    'ноябрь',
+    'декабрь',
 ]
+const month = new Date().getMonth()
+const monthKeys = [month % 11, (month + 1) % 11, (month + 2) % 11]
+const currentMonths = monthKeys.map((key) => months[key])
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .mockup {
     display: flex;
     flex-direction: column;
